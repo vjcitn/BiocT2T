d3627dc15f363f3588631bfff950413f2b696629
Vincent Carey <stvjc@rex3.bwh.harvard.edu>
2023-03-20 21:55:08 -0400

    more loci to PCA

--- R/data.R
+++ R/data.R
@@ -6,3 +6,11 @@
 #' head(pc_3kloci)
 "pc_3kloci"
 
+#' 10 PCs for 3202 samples based on .05 random sample from chr17
+#' @docType data
+#' @format matrix 3202 x 10
+#' @examples
+#' data(pc_190kloci)
+#' head(pc_190kloci)
+"pc_190kloci"
+
